2bf5c5c9bffa845da6ee9c62c50ef030b3da2773
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-01-16 18:19:21 +0300

    Adds schema of the table, fills the table and quaries to table

---
+++ data.sql
@@ -0,0 +1,5 @@
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Agumon', '3-FEB-2020', 0, '1', 10.23 ),
+('Gabumon', '15-NOV-2018', 2, '1', 8 ),
+('Pikachu', '7-JAN-2021', 1, '0',  15.04),
+('Devimon', '12-MAY-2017', 5, '1', 11 );
